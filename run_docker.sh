abc2b0aed1b383693dd09a864ea9333f0ef5d1cf
Spiryd <neumann.maks@gmail.com>
2024-04-07 12:23:53 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,5 @@ docker stop jpp
 docker rm jpp
 docker build . -t jpp --network host
 docker run --name jpp -d -i -t jpp /bin/sh
+docker rmi $(docker images --filter “dangling=true” -q --no-trunc)
 docker exec -it jpp bash
