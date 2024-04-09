bb6726929e280b651cd8b3c1fead4c638948437c
Spiryd <neumann.maks@gmail.com>
2024-04-09 06:24:42 +0200

    Update comlile_and_run.sh

--- l1/c_from_rust/comlile_and_run.sh
+++ l1/c_from_rust/comlile_and_run.sh
@@ -1,2 +1,5 @@
+cd ../rust_implementation
+cargo build --release
+cd ../c_from_rust
 gcc tester.c -o a -ll1 -L../rust_implementation/target/release
 LD_LIBRARY_PATH=../rust_implementation/target/release ./a
