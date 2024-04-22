88bf3349162f1a52f097ede19a3bdf542c98d684
Spiryd <neumann.maks@gmail.com>
2024-04-22 10:12:24 +0200

    Update Main.java

--- l2/java_implementation/Main.java
+++ l2/java_implementation/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("a + b = " + a.add(b));
         System.out.println("a * b = " + a.multiply(b));
         System.out.println("a / b = " + a.divide(b));
+        System.out.println("characteristic = " + a.characteristic());
     }
 }
 
